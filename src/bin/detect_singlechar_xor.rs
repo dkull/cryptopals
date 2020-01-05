14e6899069c8fffeaee21de36117843f2ade60b8
Tanel Liiv <tanel.liiv@defendec.com>
2020-01-05 05:22:10 +0200

    fixed warnings

--- src/bin/detect_singlechar_xor.rs
+++ src/bin/detect_singlechar_xor.rs
@@ -9,18 +9,11 @@ fn main() {
         let line = cryptopals::hex_to_bytes(line);
         let (score, key, output) = cryptopals::find_xor_key_eng(&line, 1);
         if score > best.0 && String::from_utf8(output.clone()).is_ok() {
-            /*println!(
-                "{} {} {}",
-                score,
-                key,
-                String::from_utf8(output.clone()).unwrap()
-            );*/
             best = (score, key, output);
         }
     }
     println!("best candidate is {:?}", &best);
-
-    let (score, key, bytes) = best;
+    let (_score, _key, bytes) = best;
     let text = String::from_utf8(bytes).expect("result should be text?");
     println!("text: {}", text);
 }
